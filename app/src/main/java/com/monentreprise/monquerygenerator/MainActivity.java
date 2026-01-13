93d33a6a968688c3c05a55d796fbb7ba813c3e38
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-01-13 11:14:48 +0300

    add: fonctinnalité basic

--- app/src/main/java/com/monentreprise/monquerygenerator/MainActivity.java
+++ app/src/main/java/com/monentreprise/monquerygenerator/MainActivity.java
@@ -1,23 +1,46 @@
+// package com.monentreprise.monquerygenerator;
+
+// import androidx.appcompat.app.AppCompatActivity;
+// import androidx.fragment.app.Fragment;
+// import androidx.fragment.app.FragmentManager;
+
+// import android.os.Bundle;
+
+// public class MainActivity extends AppCompatActivity {
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
+//     }
+// }
+
 package com.monentreprise.monquerygenerator;
 
+// import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import com.monentreprise.monquerygenerator.R;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-// MainActivity est la première Activity à s'exécuter
 public class MainActivity extends Activity {
-
-    // Méthode appelée lorsque l'Activity est créée (équivalent du 'main')
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Relie l'interface Java au fichier de mise en page XML
-        // R.layout.activity_main fait référence au fichier res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
-        
-        // Le code pour le TextView n'est pas nécessaire ici car le texte
-        // est déjà défini dans le fichier XML via android:text="@string/hello_world"
-    }
 
-    
+        WebView webView = (WebView) findViewById(R.id.my_webview);
+        WebSettings settings = webView.getSettings();
+        
+        // Configuration indispensable
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+
+        // Chargement du fichier local
+        webView.loadUrl("file:///android_asset/index.html");
+        // webView.loadUrl("file:///asset/index.html");
+    }
 }
